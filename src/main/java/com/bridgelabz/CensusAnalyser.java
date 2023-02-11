68474580bf7171fcd08502abe88a5ce0255d49e0
Your Name <you@example.com>
2023-02-11 21:18:06 +0530

    Welcome

---
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class CensusAnalyser {
+
+    public static void main(String[] args) {
+
+        System.out.println("....Welcome To Indian States Census Analyser Program....");
+    }
+}
